417a1e2038b811271acbcb8e9709ce49eefbdf74
hipopotamus <qkqntjddn@gmail.com>
2022-10-20 09:54:29 +0900

    BaseBy 기능 구현

--- src/main/java/initproject/global/config/AppConfig.java
+++ src/main/java/initproject/global/config/AppConfig.java
@@ -2,9 +2,15 @@ package initproject.global.config;
 
 import com.p6spy.engine.spy.P6SpyOptions;
 import initproject.global.p6spy.P6spySqlFormatConfiguration;
+import initproject.global.security.authentication.Principal;
+import initproject.global.security.authentication.UserAccount;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.annotation.PostConstruct;
+import java.util.Optional;
 
 @Configuration
 public class AppConfig {
@@ -12,5 +18,28 @@ public class AppConfig {
     @PostConstruct
     public void setLogMessageFormat() {
         P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfiguration.class.getName());
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+
+        return new AuditorAware<String>() {
+
+            @Override
+            public Optional<String> getCurrentAuditor() {
+
+                if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                    return Optional.of("nonAuthentication");
+                }
+
+                Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+                if (principal == "anonymousUser") {
+                    return Optional.of(principal.toString());
+                }
+
+                return Optional.of(((Principal) principal).getEmail());
+            }
+        };
     }
 }
